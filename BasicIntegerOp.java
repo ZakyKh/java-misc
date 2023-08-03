72ecaa5d90e18d07ad9d6c468261d655bc299f2c
ZakyKh <mzakykhairuddin@gmail.com>
2023-08-03 20:38:25 +0700

    add basic integer operation program

---
+++ BasicIntegerOp.java
@@ -0,0 +1,11 @@
+public class BasicIntegerOp {
+	public static void main(String []args) {
+		int x = 12;
+		int y = 25;
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+		System.out.println(x % y);
+	}
+}
